3f53307dcde29e994c7280a52a4ba0586c4fe401
Mark Arneman <markarneman@gmail.com>
2019-02-03 16:17:46 -0500

    cleanup and changes

--- Assets/_G3/Scripts/SceneControl.cs
+++ Assets/_G3/Scripts/SceneControl.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour {
 
+	private void Start() {
+		
+	}
+
+	public void BeginShow() {
+		Debug.Log("Lets start the show!");
+	}
+
+	// TODO: Show Credits
+	public void ShowCredits() {
+		Debug.Log("Show credits");
+	}
+
 	public void StartQuestions() {
 		SceneManager.LoadScene("Questions");
 	}
