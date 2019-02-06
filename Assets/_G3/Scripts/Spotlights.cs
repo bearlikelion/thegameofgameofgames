8c808de918dd5d307d593672f365d4f92d6ffab7
Mark Arneman <markarneman@gmail.com>
2019-02-05 23:11:00 -0500

    Center blue spotlight

--- Assets/_G3/Scripts/Spotlights.cs
+++ Assets/_G3/Scripts/Spotlights.cs
@@ -9,13 +9,20 @@ public class Spotlights : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		blueLight.transform.DOLocalRotate(new Vector3(0, 0, 15f), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();		
-		redLight.transform.DOLocalRotate(new Vector3(0, 0, 30), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();
-		yellowLight.transform.DOLocalRotate(new Vector3(0, 0, -30f), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();
+		// blueLight.transform.DOLocalRotate(new Vector3(0, 0, 15f), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();		
+		// redLight.transform.DOLocalRotate(new Vector3(0, 0, 30), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();
+		// yellowLight.transform.DOLocalRotate(new Vector3(0, 0, -30f), 25f).SetLoops(-1, LoopType.Yoyo).SetSpeedBased();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// blueLight.transform.rotation = Quaternion.Euler(0f, 0f, 15f * Mathf.Sin(Time.time * 2f));
+		float blueAngle = Mathf.PingPong(Time.time * 15f, 30) - 15;		
+		blueLight.transform.rotation = Quaternion.Euler(0, 0, blueAngle);
+
+		float yellowAngle = Mathf.PingPong(Time.time * 15f, 30) ;
+		yellowLight.transform.rotation = Quaternion.Euler(0, 0, -yellowAngle);	
+
+		float redAngle = Mathf.PingPong(Time.time * 15f, 30);	
+		redLight.transform.rotation = Quaternion.Euler(0, 0, redAngle);
 	}
 }
