23ff979ccf15c380bea0d7a5a9029fdfb5e0b55b
Mark Arneman <mark@arneman.me>
2019-04-16 13:22:03 -0400

    Slowed down intro

--- Assets/_G3/Scripts/Secret.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Secret {
-    static public string PublicKey {
-        get { return "5c5dd477b639870c248d3350"; }
-    }
-
-    static public string PrivateKey {
-        get { return "Kq1A13T8Lk2ZwYL80nJ4dAxw1uouf87U6lGhI6v3SOjw"; }
-    }
-}
